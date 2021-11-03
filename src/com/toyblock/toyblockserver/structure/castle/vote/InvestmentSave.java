bc0993bdd76f12dd1a0c2aa59acd6614338b07d6
toyblock2000 <84675056+toyblock2000@users.noreply.github.com>
2021-11-03 14:50:57 +0900

    2021-11-02 회사

--- src/com/toyblock/toyblockserver/structure/castle/vote/InvestmentSave.java
+++ src/com/toyblock/toyblockserver/structure/castle/vote/InvestmentSave.java
@@ -1,16 +1,30 @@
 package com.toyblock.toyblockserver.structure.castle.vote;
 
-import java.io.*;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public class InvestmentSave {
+import java.io.*;
+import java.util.HashMap;
+
+public class InvestmentSave extends JavaPlugin {
     private final File f = new File(getDataFolder(), "/data.txt");
+
+    public void makeFile(File f) {
+        if (!f.exists() || !f.isFile()) {
+            try {
+                f.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public void fileToMap(File f, HashMap<String, Integer> map) {
         try {
             BufferedReader reader = new BufferedReader(new FileReader(f));
             String fileLine = null;
             while ((fileLine = reader.readLine()) != null) {
 
-                String uuid = String.fromString(fileLine.split("\\|")[0]);
+                String uuid = fileLine.split("\\|")[0];
                 String str = fileLine.split("\\|")[1];
 
                 map.put(uuid, Integer.parseInt(str));
@@ -19,6 +33,17 @@ public class InvestmentSave {
             e3.printStackTrace();
         } catch (IOException e4) {
             e4.printStackTrace();
+        }
+    }
+    public void mapToFile(File f, HashMap<String, Integer> map) {
+        try {
+            FileWriter writer = new FileWriter(f, false);
+            for(String uuid : map.keySet()){
+                writer.write(uuid+"|"+map.get(uuid)+"\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
