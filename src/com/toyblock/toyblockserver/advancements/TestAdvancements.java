040e44761997472273af4e79c0f875665fb3fa42
toyblock2000 <84675056+toyblock2000@users.noreply.github.com>
2021-11-09 18:07:14 +0900

    2021-11-02 회사

--- src/com/toyblock/toyblockserver/advancements/TestAdvancements.java
+++ src/com/toyblock/toyblockserver/advancements/TestAdvancements.java
@@ -7,11 +7,15 @@ import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitScheduler;
+import org.bukkit.scheduler.BukkitTask;
 
 import javax.swing.text.JTextComponent;
 import java.awt.*;
 
-public class TestAdvancements {
+public class TestAdvancements extends JavaPlugin {
     public void test(Player player) {
         AdvancementManager manager = new AdvancementManager();
         AdvancementDisplay rootDisplay = new AdvancementDisplay(Material.BEDROCK, "How...?", "§5Collect Bedrock", AdvancementDisplay.AdvancementFrame.CHALLENGE, true, true, AdvancementVisibility.ALWAYS);
@@ -21,5 +25,11 @@ public class TestAdvancements {
         root.displayMessageToEverybody(player);
         manager.addAdvancement(root);
         manager.grantAdvancement(player, root);
+        Bukkit.getScheduler().runTaskTimer(this, new Runnable() {
+            @Override
+            public void run() {
+                manager.addPlayer(player);
+            }
+        },3,3);
     }
 }
