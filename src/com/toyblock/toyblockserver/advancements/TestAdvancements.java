c3e1d1c7efb27eafce2c07643762e3eacb6f6c6f
toyblock2000 <84675056+toyblock2000@users.noreply.github.com>
2021-11-09 17:20:13 +0900

    2021-11-02 회사

--- src/com/toyblock/toyblockserver/advancements/TestAdvancements.java
+++ src/com/toyblock/toyblockserver/advancements/TestAdvancements.java
@@ -4,6 +4,7 @@ import eu.endercentral.crazy_advancements.*;
 import eu.endercentral.crazy_advancements.manager.AdvancementManager;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
@@ -12,17 +13,13 @@ import java.awt.*;
 
 public class TestAdvancements {
     public void test(Player player) {
-        AdvancementManager manager = new AdvancementManager(player);
-        net.md_5.bungee.api.chat.TextComponent message = new TextComponent("Hello world");
-        message.setColor(net.md_5.bungee.api.ChatColor.RED);
-        message.setBold(true);
-        AdvancementDisplay rootDisplay = new AdvancementDisplay(Material.ARMOR_STAND, "My Custom Advancements", "With cool additions", AdvancementDisplay.AdvancementFrame.TASK, false, false, AdvancementVisibility.ALWAYS);
-        rootDisplay.setBackgroundTexture("textures/block/yellow_concrete.png");
+        AdvancementManager manager = new AdvancementManager();
+        AdvancementDisplay rootDisplay = new AdvancementDisplay(Material.BEDROCK, "How...?", "§5Collect Bedrock", AdvancementDisplay.AdvancementFrame.CHALLENGE, true, true, AdvancementVisibility.ALWAYS);
+        rootDisplay.setBackgroundTexture("textures/block/compass.png");
         Advancement root = new Advancement(null, new NameKey("custom", "root"), rootDisplay);
-
-        AdvancementDisplay childrenDisplay = new AdvancementDisplay(Material.ENDER_EYE, "오른쪽으로", "당신의 목표", AdvancementDisplay.AdvancementFrame.GOAL, true, true, AdvancementVisibility.VANILLA);
-        childrenDisplay.setCoordinates(1, 0);//x, y
-        Advancement children = new Advancement(root, new NameKey("custom", "right"), childrenDisplay);
-        AdvancementDisplay display = new AdvancementDisplay(Material.ANVIL, new JSONMessage(message), new JSONMessage(message), AdvancementDisplay.AdvancementFrame.GOAL, true, true, AdvancementVisibility.VANILLA);
+        root.setCriteria(1);
+        root.displayMessageToEverybody(player);
+        manager.addAdvancement(root);
+        manager.grantAdvancement(player, root);
     }
 }
