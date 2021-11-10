6394aeb9a5b69768cf97a96a51591fbc643c323f
toyblock2000 <84675056+toyblock2000@users.noreply.github.com>
2021-11-10 11:26:59 +0900

    2021-11-02 회사

--- src/com/toyblock/toyblockserver/advancements/list/Adventurer.java
+++ src/com/toyblock/toyblockserver/advancements/list/Adventurer.java
@@ -5,7 +5,6 @@ import eu.endercentral.crazy_advancements.AdvancementDisplay;
 import eu.endercentral.crazy_advancements.AdvancementVisibility;
 import eu.endercentral.crazy_advancements.NameKey;
 import eu.endercentral.crazy_advancements.manager.AdvancementManager;
-import io.lumine.xikage.mythicmobs.players.PlayerManager;
 import org.bukkit.Material;
 import org.bukkit.Statistic;
 import org.bukkit.entity.Player;
@@ -23,22 +22,25 @@ public class Adventurer implements Listener {
 
     public static void createAdventurer () {
         AdvancementDisplay startDisplay =
-                new AdvancementDisplay(Material.DARK_OAK_SAPLING, "모험가", "§5모험을 시작합니다", AdvancementDisplay.AdvancementFrame.CHALLENGE, true, true, AdvancementVisibility.ALWAYS);
+                new AdvancementDisplay(Material.DARK_OAK_SAPLING, "모험가", "§5모험을 시작합니다", AdvancementDisplay.AdvancementFrame.CHALLENGE, false, true, AdvancementVisibility.ALWAYS);
         startDisplay.setBackgroundTexture("textures/block/azalea_plant.png");
+
         Advancement start = new Advancement(null, new NameKey("adventurer", "start"), startDisplay);
         start.setCriteria(1);
         manager.addAdvancement(start);
 
         AdvancementDisplay move1_Display =
-                new AdvancementDisplay(Material.GRASS_BLOCK, "탐험중", "§5100블럭을 탐험 하세요", AdvancementDisplay.AdvancementFrame.CHALLENGE, true, true, AdvancementVisibility.ALWAYS);
+                new AdvancementDisplay(Material.GRASS_BLOCK, "탐험중", "§5100블럭을 탐험 하세요", AdvancementDisplay.AdvancementFrame.CHALLENGE, false, true, AdvancementVisibility.PARENT_GRANTED);
         move1_Display.setCoordinates(1,0);
+
         Advancement move1 = new Advancement(start, new NameKey("adventurer", "move1"), move1_Display);
-        move1.setCriteria(1);
+        move1.setCriteria(100);
         manager.addAdvancement(move1);
 
         AdvancementDisplay move2_Display =
-                new AdvancementDisplay(Material.GRASS_BLOCK, "탐험중2", "§5200블럭을 탐험 하세요", AdvancementDisplay.AdvancementFrame.CHALLENGE, true, true, AdvancementVisibility.ALWAYS);
+                new AdvancementDisplay(Material.GRASS_BLOCK, "탐험중2", "§5200블럭을 탐험 하세요", AdvancementDisplay.AdvancementFrame.CHALLENGE, false, true, AdvancementVisibility.PARENT_GRANTED);
         move2_Display.setCoordinates(2,1);
+
         Advancement move2 = new Advancement(move1, new NameKey("adventurer", "move2"), move2_Display);
         move2.setCriteria(1);
         manager.addAdvancement(move2);
@@ -57,8 +59,29 @@ public class Adventurer implements Listener {
         }
             Advancement advancement;
             advancement = manager.getAdvancement(new NameKey("adventurer","move1"));
-            manager.grantAdvancement(event.getPlayer(),advancement);
-            manager.get
+            manager.setCriteriaProgress(event.getPlayer(),advancement,manager.getCriteriaProgress(event.getPlayer(),advancement)+1);
+
+
 
     }
+    @EventHandler
+    public static void move2up(PlayerInteractEvent event) {
+        if(!event.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
+            return;
+        }
+        Advancement advancement;
+        advancement = manager.getAdvancement(new NameKey("adventurer","move2"));
+        Advancement advancementup;
+        advancementup = manager.getAdvancement(new NameKey("adventurer","move1"));
+        if(!(manager.getCriteriaProgress(event.getPlayer(),advancementup)>=100)) {
+            event.getPlayer().chat(""+manager.getCriteriaProgress(event.getPlayer(),advancementup));
+            return;
+        }
+        manager.setCriteriaProgress(event.getPlayer(),advancement,manager.getCriteriaProgress(event.getPlayer(),advancement)+1);
+
+
+
+    }
+
+
 }
