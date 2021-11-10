9015154e39f401995681d3da4ebb189443ef17ad
toyblock2000 <84675056+toyblock2000@users.noreply.github.com>
2021-11-10 13:53:22 +0900

    2021-11-02 회사

--- src/com/toyblock/toyblockserver/structure/castle/worldguard/MakeRegion.java
+++ src/com/toyblock/toyblockserver/structure/castle/worldguard/MakeRegion.java
@@ -1,6 +1,8 @@
 package com.toyblock.toyblockserver.structure.castle.worldguard;
 
 import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldguard.protection.flags.Flags;
+import com.sk89q.worldguard.protection.flags.StateFlag;
 import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import org.bukkit.Location;
@@ -10,6 +12,6 @@ public class MakeRegion {
         BlockVector3 min = BlockVector3.at(loc.getBlockX()+2,loc.getBlockY()+5,loc.getBlockZ()+2);
         BlockVector3 max = BlockVector3.at(loc.getBlockX()-2,loc.getBlockY()-4,loc.getBlockZ()-2);
         ProtectedRegion region = new ProtectedCuboidRegion(name,min,max);
-        region.getFlag(Flag.)
+        region.setFlag(Flags.BLOCK_BREAK, StateFlag.State.DENY);
     }
 }
