e52d3058c7cfc04c11fe502b6fd2d567f03b1e0e
toyblock2000 <84675056+toyblock2000@users.noreply.github.com>
2021-11-10 17:14:55 +0900

    2021-11-02 회사

--- src/com/toyblock/toyblockserver/inventory/dropchance/dropchance.java
+++ src/com/toyblock/toyblockserver/inventory/dropchance/dropchance.java
@@ -2,6 +2,8 @@ package com.toyblock.toyblockserver.inventory.dropchance;
 
 import locate.tool;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -10,9 +12,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class dropchance {
-    public void itemChance(ItemStack item) {
+    public int itemChance(ItemStack item) {
         String lore =item.getItemMeta().getLore().get(5);
         lore = lore.replaceAll("[^0-9]","");
         int chance = Integer.parseInt(lore);
+        return chance;
     }
+    public boolean chance(double probabilityTrue) {
+        return Math.random()*100 >= 100 - probabilityTrue;
     }
+    public void drop(Inventory inv) {
+        int nomber =0;
+        for (int i = 0;i<=35;i++) {
+            int chance = itemChance(inv.getItem(i));
+            if(chance(chance)) {
+                inv.(i);
+            }
+        }
+    }
+    EventHandler
+    public void death(PlayerDeathEvent event) {
+        event.get
+    }
+}
