e23fc25a255be08fbfcb4eacbac60d0a05420593
toyblock2000 <84675056+toyblock2000@users.noreply.github.com>
2021-11-10 16:00:35 +0900

    2021-11-02 회사

--- src/com/toyblock/toyblockserver/structure/castle/worldguard/MakeRegion.java
+++ src/com/toyblock/toyblockserver/structure/castle/worldguard/MakeRegion.java
@@ -1,10 +1,12 @@
 package com.toyblock.toyblockserver.structure.castle.worldguard;
 
 import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.world.World;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.protection.flags.Flags;
 import com.sk89q.worldguard.protection.flags.StateFlag;
 import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.managers.storage.StorageException;
 import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import com.sk89q.worldguard.protection.regions.RegionContainer;
@@ -16,8 +18,13 @@ public class MakeRegion {
         BlockVector3 max = BlockVector3.at(loc.getBlockX()-2,loc.getBlockY()-4,loc.getBlockZ()-2);
         ProtectedRegion region = new ProtectedCuboidRegion(name,min,max);
         RegionContainer container = WorldGuard.getInstance().getPlatform().getRegionContainer();
-        RegionManager regions = container.get(world);
+        RegionManager regions = container.get((World) loc.getWorld());
         regions.addRegion(region);
         region.setFlag(Flags.BLOCK_BREAK, StateFlag.State.DENY);
+        try {
+            regions.save();
+        } catch (StorageException e) {
+            e.printStackTrace();
+        }
     }
 }
