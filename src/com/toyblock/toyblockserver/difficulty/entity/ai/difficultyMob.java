44d7a4cb68dccf45bd2ff9ec4035ce7836d5fa15
toyblock2000 <rmsdnjsdl23@naver.com>
2022-02-24 20:12:23 +0900

    11월28일 집

--- src/com/toyblock/toyblockserver/difficulty/entity/ai/difficultyMob.java
+++ src/com/toyblock/toyblockserver/difficulty/entity/ai/difficultyMob.java
@@ -1,11 +1,10 @@
 package com.toyblock.toyblockserver.difficulty.entity.ai;
 
 import com.sk89q.worldguard.bukkit.event.entity.SpawnEntityEvent;
+import com.toyblock.toyblockserver.Main;
 import com.toyblock.toyblockserver.tool.developer.bug;
 import org.bukkit.attribute.Attribute;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Zombie;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
@@ -21,31 +20,60 @@ public class difficultyMob implements Listener {
     public void zombieDamage_x2(Zombie mob) {
         mob.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE).setBaseValue(4.5*2);
     }
+
     public void zombieMoveSpeed_x2(Zombie mob) {
         mob.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(0.23*2);
     }
     @EventHandler
     public void nomalSpawn(EntitySpawnEvent event) {
-        if(!(event.getEntity().getType().equals(EntityType.ZOMBIE))) {
-            return;
+        if(event.getEntity().getType().equals(EntityType.ZOMBIE)) {
+            Zombie mob = (Zombie) event.getEntity();
+            zombieTear1(mob);
         }
-        Zombie mob = (Zombie) event.getEntity();
-        randomSpawn(mob);
+        if(event.getEntity().getType().equals(EntityType.SKELETON)) {
+            Skeleton mob = (Skeleton) event.getEntity();
+            skeletonTear1(mob);
+        }
+        if(event.getEntity().getType().equals(EntityType.CREEPER)) {
+            Creeper mob = (Creeper) event.getEntity();
+            creeperTear1(mob);
+        }
+
     }
-    public void randomSpawn(Zombie mob) {
+    public void zombieTear1(Zombie mob) {
+        mobList list = new mobList();
         Random random = new Random();
-        int value = random.nextInt(3);
-        if(value==0) {
-            zombieDamage_x2(mob);
-            bug.chat("데미지");
-        }
+        int value = random.nextInt(4);
         if(value==1) {
-            zombieHurt_x2(mob);
-            bug.chat("체력");
+            list.zombieDamage_x05(mob);
         }
         if(value==2) {
-            zombieMoveSpeed_x2(mob);
-            bug.chat("속도");
+            list.zombieHurt_x05(mob);
+        }
+        if(value==3) {
+            list.zombieMoveSpeed_x05(mob);
+        }
+    }
+    public void skeletonTear1(Skeleton mob) {
+        mobList list = new mobList();
+        Random random = new Random();
+        int value = random.nextInt(3);
+        if(value==1) {
+            list.skeletonHurt_x05(mob);
+        }
+        if(value==2) {
+            list.skeletonMoveSpeed_x05(mob);
+        }
+    }
+    public void creeperTear1(Creeper mob) {
+        mobList list = new mobList();
+        Random random = new Random();
+        int value = random.nextInt(3);
+        if(value==1) {
+            list.creeperSlient(mob);
+        }
+        if(value==2) {
+            list.creeperMoveSpeed_x05(mob);
         }
     }
 }
